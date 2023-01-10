d452c6db67412ef465912776c443972e2a277ebb
Fredrik <fredrikf2000@gmail.com>
2023-01-10 11:38:01 +0100

    added nav and made it responsive

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <Nav />
     </div>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,29 @@
 import React from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { useRef } from "react";
 
 const Nav = () => {
+  const navRef = useRef();
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsiveNav");
+  };
+
   return (
     <header>
       <h3>Flyxo</h3>
-      <nav>
+      <nav ref={navRef}>
+        <button onClick={showNavbar} className="nav-btn nav-close-btn">
+          <FaTimes />
+        </button>
         <a href="/">Home</a>
         <a href="/">Discography</a>
         <a href="/">About</a>
         <a href="/">Contact</a>
-        <button></button>
       </nav>
+      <button onClick={showNavbar} className="nav-btn">
+        <FaBars />
+      </button>
     </header>
   );
 };
