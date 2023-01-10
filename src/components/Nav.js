2a4afaf04b75681bd35950c062d5dc700dbd836a
Fredrik <fredrikf2000@gmail.com>
2023-01-10 11:10:32 +0100

    installed react-icons and started working on nav

---
+++ src/components/Nav.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <header>
+      <h3>Flyxo</h3>
+      <nav>
+        <a href="/">Home</a>
+        <a href="/">Discography</a>
+        <a href="/">About</a>
+        <a href="/">Contact</a>
+        <button></button>
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
