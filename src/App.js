0db000813eb8f67a7a432a1765202eba77e73fd0
Fredrik <fredrikf2000@gmail.com>
2023-01-12 16:08:21 +0100

    started working on contact section & changed tag

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Nav from "./components/Nav";
 import Discography from "./components/Discography";
 import About from "./components/About";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Nav />
       <Discography />
       <About />
+      <Contact />
     </div>
   );
 }
